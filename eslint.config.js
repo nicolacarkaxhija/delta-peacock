d10e84b44d284e1e0e38b4e8e772978a914ab1a9
Nicola Carkaxhija <52464255+nicolacarkaxhija@users.noreply.github.com>
2026-07-09 23:58:09 +0200

    chore: lint, format and commit hygiene tooling

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import js from "@eslint/js";
+import prettier from "eslint-config-prettier";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  { ignores: ["dist/", "coverage/", "node_modules/"] },
+  js.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  { files: ["**/*.{js,mjs,cjs}"], ...tseslint.configs.disableTypeChecked },
+  prettier,
+);
